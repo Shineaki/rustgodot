2f7392db6793ca27a38d7a11057dfc097440ade0
Shineaki <halmosadam92@gmail.com>
2025-03-06 22:59:06 +0100

    feat: :poop: Cloud function to init db after user create

--- client/Scripts/CharacterSelect.gd
+++ client/Scripts/CharacterSelect.gd
@@ -3,7 +3,6 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
 	print("Fetching " + UserData.user_id)
 	var collection = Firebase.Firestore.collection('characters')
 	var document = await collection.get_doc(UserData.user_id)
