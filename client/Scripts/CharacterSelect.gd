987ebafcf2998231a49773210e119b1dbca50570
Shineaki <halmosadam92@gmail.com>
2025-03-09 19:15:13 +0100

    fix: :bug: Some UI Changes

--- client/Scripts/CharacterSelect.gd
+++ client/Scripts/CharacterSelect.gd
@@ -3,17 +3,21 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$SpinnerBox.visible = true
+	$PlayContainer.visible = false
+	$CharacterContainer.visible = false
 	print("Fetching " + UserData.user_id)
-	var scene = load("res://Scenes/player_container.tscn")
+	var scene = load("res://Scenes/PlayerContainer.tscn")
 	var collection = Firebase.Firestore.collection('characters')
 	var document = await collection.get_doc(UserData.user_id)
 	for value in document.keys():
 		var pci = scene.instantiate()
 		pci.set_player_name(document.get(value).get("name"))
-		pci.set_player_level(str(document.get(value).get("level")))
-		$VBoxContainer/VBoxContainer/HBoxContainer/VBoxContainer.add_child(pci)
+		pci.set_player_level("Level " + str(document.get(value).get("level")))
+		$CharacterContainer/VBoxContainer.add_child(pci)
 	$SpinnerBox.visible = false
-	$VBoxContainer.visible = true
+	$PlayContainer.visible = true
+	$CharacterContainer.visible = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
