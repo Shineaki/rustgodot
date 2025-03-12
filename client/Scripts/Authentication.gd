fb4d8aefa9121679050b9542777a6909a5ed9651
Shineaki <halmosadam92@gmail.com>
2025-03-13 00:23:41 +0100

    feat: :lipstick: Logout

--- client/Scripts/Authentication.gd
+++ client/Scripts/Authentication.gd
@@ -4,6 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# https://github.com/GodotNuts/GodotFirebase/wiki/Authentication-and-User-Management#save-encrypted-auth-file
+	$FormContainer.visible = false
+	$SpinnerBox.visible = true
 	Firebase.Auth.login_succeeded.connect(on_login_succeeded)
 	Firebase.Auth.signup_succeeded.connect(on_signup_succeeded)
 	Firebase.Auth.login_failed.connect(on_login_failed)
@@ -11,53 +13,58 @@ func _ready() -> void:
 	
 	if Firebase.Auth.check_auth_file():
 		print("Auth file exists, loading it ...")
+	else:
+		$FormContainer.visible = true
+		$SpinnerBox.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _on_login_button_pressed() -> void:
-	var email = $VBoxContainer/EmailInput.text
-	var password = $VBoxContainer/PasswordInput.text
-	$VBoxContainer/EmailInput.editable = false
-	$VBoxContainer/PasswordInput.editable = false
+	var email = $FormContainer/EmailInput.text
+	var password = $FormContainer/PasswordInput.text
+	$FormContainer/EmailInput.editable = false
+	$FormContainer/PasswordInput.editable = false
 	Firebase.Auth.login_with_email_and_password(email, password)
-	$VBoxContainer/StateLabel.text = "Logging in"
-	$VBoxContainer/ErrorLabel.text = ""
+	$FormContainer/StateLabel.text = "Logging in"
+	$FormContainer/ErrorLabel.text = ""
 
 func _on_signup_button_pressed() -> void:
-	var email = $VBoxContainer/EmailInput.text
-	var password = $VBoxContainer/PasswordInput.text
-	$VBoxContainer/EmailInput.editable = false
-	$VBoxContainer/PasswordInput.editable = false
+	var email = $FormContainer/EmailInput.text
+	var password = $FormContainer/PasswordInput.text
+	$FormContainer/EmailInput.editable = false
+	$FormContainer/PasswordInput.editable = false
 	Firebase.Auth.signup_with_email_and_password(email, password)
-	$VBoxContainer/StateLabel.text = "Signing Up"
-	$VBoxContainer/ErrorLabel.text = ""
+	$FormContainer/StateLabel.text = "Signing Up"
+	$FormContainer/ErrorLabel.text = ""
 
 func on_login_succeeded(auth):
 	print("on_login_succeeded")
-	$VBoxContainer/StateLabel.text = "Login Success!"
-	$VBoxContainer/ErrorLabel.text = ""
-	$VBoxContainer/EmailInput.editable = true
-	$VBoxContainer/PasswordInput.editable = true
+	$FormContainer/StateLabel.text = "Login Success!"
+	$FormContainer/ErrorLabel.text = ""
+	$FormContainer/EmailInput.editable = true
+	$FormContainer/PasswordInput.editable = true
 	Globals.usr_data = auth
 	Firebase.Auth.save_auth(auth)
 	get_tree().change_scene_to_file("res://Scenes/CharacterSelect.tscn")
 	
 func on_signup_succeeded(auth):
-	$VBoxContainer/StateLabel.text = "Sign Up Success!"
-	$VBoxContainer/ErrorLabel.text = ""
-	$VBoxContainer/EmailInput.editable = true
-	$VBoxContainer/PasswordInput.editable = true
+	$FormContainer/StateLabel.text = "Sign Up Success!"
+	$FormContainer/ErrorLabel.text = ""
+	$FormContainer/EmailInput.editable = true
+	$FormContainer/PasswordInput.editable = true
 
 func on_login_failed(error_code, message):
-	$VBoxContainer/StateLabel.text = "Login Failed!"
-	$VBoxContainer/ErrorLabel.text = message
-	$VBoxContainer/EmailInput.editable = true
-	$VBoxContainer/PasswordInput.editable = true
+	$FormContainer/StateLabel.text = "Login Failed!"
+	$FormContainer/ErrorLabel.text = message
+	$FormContainer.visible = true
+	$SpinnerBox.visible = false
+	$FormContainer/EmailInput.editable = true
+	$FormContainer/PasswordInput.editable = true
 
 func on_signup_failed(error_code, message):
-	$VBoxContainer/StateLabel.text = "Sign Up Failed!"
-	$VBoxContainer/ErrorLabel.text = message
-	$VBoxContainer/EmailInput.editable = true
-	$VBoxContainer/PasswordInput.editable = true
+	$FormContainer/StateLabel.text = "Sign Up Failed!"
+	$FormContainer/ErrorLabel.text = message
+	$FormContainer/EmailInput.editable = true
+	$FormContainer/PasswordInput.editable = true
--- client/Scripts/CharacterSelect.gd
+++ client/Scripts/CharacterSelect.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# TODO: Logout
-# Firebase.Auth.logout()
-# get_tree().change_scene_to_file("res://Scenes/Authentication.tscn")
-
 func api_request(endpoint: String, callback: Callable) -> void:
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
@@ -38,11 +34,16 @@ func _get_character_list_completed(result, response_code, headers, body):
 	var scene = load("res://Scenes/PlayerContainer.tscn")
 	
 	var response = self.parse_response(body)
+	var characters = 0
 	for value in response.keys():
+		characters += 1
 		var pci = scene.instantiate()
 		pci.set_player_name(response.get(value).get("name"))
 		pci.set_player_level("Level " + str(response.get(value).get("level")))
 		$CharacterContainer/VBoxContainer.add_child(pci)
+	
+	if characters >= 4: # NUMBER_OF_MAX_CHARACTERS_PER_ACCOUNT
+		$CharacterContainer/VBoxContainer/CreateNewCharacterButton.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -57,3 +58,7 @@ func _http_request_completed(result, response_code, headers, body):
 		push_error("API call failed")
 	var response = self.parse_response(body)
 	print(response)
+
+func _on_logout_button_pressed() -> void:
+	Firebase.Auth.logout()
+	get_tree().change_scene_to_file("res://Scenes/Authentication.tscn")
